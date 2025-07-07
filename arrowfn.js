a654d1af7d28f63859d6e59ab77569abefad5c49
aneeshrao1234 <166586195+aneeshrao1234@users.noreply.github.com>
2025-07-07 10:40:36 +0530

    7.7

---
+++ arrowfn.js
@@ -0,0 +1,10 @@
+// Traditional function
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+// Arrow function equivalent 
+const greetArrow = (name) => `hello, ${name}!`;
+
+// Usage
+console.log(greet("Farheen"));
+console.log(greeetArrow("Farheen"));
---
+++ arrowfn2.js
@@ -0,0 +1,6 @@
+const checkOddEvenArrow = (num) => {
+    console.log(num = (num % 2 === 0 ? " is Even ": " is Odd"));
+};
+// Example usage 
+checkOddEvenArrow(12);
+checkOddEvenArrow(11);
